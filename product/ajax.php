9c9ce099694ceaad8252fe467c30416cedea8ac1
MuhamadSholikhudin <41666877+MuhamadSholikhudin@users.noreply.github.com>
2022-09-12 06:02:50 +0700

    Seacrh_product

--- product/ajax.php
+++ product/ajax.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 include '../function.php';
 
 if (isset($_POST['product_id'])) {
@@ -16,21 +16,68 @@ if (isset($_POST['product_id'])) {
     mysqli_query($koneksi, $query);
     echo json_encode($query);
 }
+// Search product
+elseif(isset($_POST['search_product'])){
+
+//Cari product
+
+$count_product = querysatudata(" SELECT COUNT(id) as count FROM products WHERE  name_product LIKE '%".$_POST['search_product']."%' OR barcode LIKE '%".$_POST['search_product']."%' "); 
+if($count_product['count'] > 0){
+
+$product_loop = '';
+$products = querybanyak("SELECT *  products  
+                    WHERE LIKE name_product '%".$_POST['search_product']."%' OR LIKE barcode '%".$_POST['search_product']."%' 
+                    LIMIT 20");
+
+                    foreach($products as $product){
+                    $product_loop .= '
+                    <tr>
+                      <td>'.$product['name_product'] .'</td>
+                      <td>'.$product['abbreviation'].'</td>
+                      <td>'.$product['barcode'].'</td>
+                      <td>'.$product['price'].'</td>
+                      <td>
+                        <a href="'.Base_url('index.php?page=products_edit&product_id=') . $product['id'] .'" class="btn btn-sm btn-success" >
+                          <i class="mdi mdi-grease-pencil"></i>
+                        </a>
+
+                        <a href="" class="btn btn-sm btn-success" >
+                          <i class="mdi mdi-eye"></i>
+                        </a>
+                      </td>
+                    </tr>';
+                    
+                    }
+
+}else{
+$product_loop = '<tr>
+<td> </td>
+<td> </td>
+<td> Tidak ada </td>
+<td> </td>
+<td> </td>
+</tr>';
+}
+
+echo json_encode($product_loop);
+}
+
+
 elseif(isset($_POST['price_id'])){
 
     $query = "DELETE FROM productprices WHERE id = ".$_POST['price_id']."";
     mysqli_query($koneksi, $query);
     echo json_encode($query);
         
-}elseif (isset($_POST['name_product']) && isset($_POST['barcode']) && isset($_POST['abbreviation'])) {
+}elseif (isset($_POST['name_product']) && isset($_POST['barcode']) && isset($_POST['abbreviation']) && isset($_POST['price'])) {
 
     $created_at = date("Y-m-d H:i:s");
 
     $updated_at = $created_at;
 
-    $query = "INSERT INTO products (name_product, abbreviation, barcode, created_at, updated_at) VALUES 
+    $query = "INSERT INTO products (name_product, abbreviation, barcode, price, created_at, updated_at) VALUES 
     (
-        '" . $_POST['name_product'] . "', '" . $_POST['abbreviation'] . "', '" . $_POST['barcode'] . "', '" . $created_at . "', '" . $updated_at . "'
+        '" . $_POST['name_product'] . "', '" . $_POST['abbreviation'] . "', '" . $_POST['barcode'] . "', ".$_POST['price']." ,'" . $created_at . "', '" . $updated_at . "'
     )";
 
     mysqli_query($koneksi, $query);
